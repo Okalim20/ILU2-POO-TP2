3a048af4b8661a43dd51bb1a9d4e93a45462ca1f
Okalim20 <liam.duneau-delis@laposte.net>
2026-03-19 19:44:24 +0100

    TP2-2 à TP2-7 terminé

--- src/frontiere/BoundaryAfficherMarche.java
+++ src/frontiere/BoundaryAfficherMarche.java
@@ -10,6 +10,25 @@ public class BoundaryAfficherMarche {
 	}
 
 	public void afficherMarche(String nomAcheteur) {
-
+		String[] infosMarche = controlAfficherMarche.donnerInfosMarche();
+		if (infosMarche.length==0) {
+			System.out.println("Le marché est vide, revenez plus tard.");
+		} else {
+			StringBuilder message = new StringBuilder();
+			System.out.println(nomAcheteur+", vous trouverez au marché :");
+			String vendeur;
+			String quantite;
+			String produit;
+			for (int i = 0; i < infosMarche.length;) {
+				vendeur = infosMarche[i];
+				i++;
+				quantite = infosMarche[i];
+				i++;
+				produit = infosMarche[i];
+				i++;
+				message.append("- "+vendeur+" qui vend "+quantite+" "+produit);
+			}
+			System.out.println(message.toString());
+		}
 	}
 }
